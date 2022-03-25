a8535294740cac47af4a9619743333cdca8b2071
Miguel Rodrigues <mcpr.inf@gmail.com>
2022-03-25 01:51:21 +0000

    FirstCommit

---
+++ NotesReminder/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace NotesReminder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.richTextBoxContent = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.monthCalendarDate = new System.Windows.Forms.MonthCalendar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonAdd.Location = new System.Drawing.Point(7, 52);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 28);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Adicionar";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // richTextBoxContent
+            // 
+            this.richTextBoxContent.Location = new System.Drawing.Point(90, 12);
+            this.richTextBoxContent.Name = "richTextBoxContent";
+            this.richTextBoxContent.Size = new System.Drawing.Size(227, 163);
+            this.richTextBoxContent.TabIndex = 5;
+            this.richTextBoxContent.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(10, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 38);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Conteudo \r\nda Nota";
+            // 
+            // monthCalendarDate
+            // 
+            this.monthCalendarDate.Location = new System.Drawing.Point(90, 187);
+            this.monthCalendarDate.Name = "monthCalendarDate";
+            this.monthCalendarDate.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 41);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Mostrar\r\nNotas\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "NoteReminder";
+            this.notifyIcon1.BalloonTipTitle = "NoteReminder";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "NoteReminder";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 357);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.monthCalendarDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBoxContent);
+            this.Controls.Add(this.buttonAdd);
+            this.Name = "Form1";
+            this.Text = "NoteReminder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button buttonAdd;
+        private RichTextBox richTextBoxContent;
+        private Label label2;
+        private MonthCalendar monthCalendarDate;
+        private Button button1;
+        private NotifyIcon notifyIcon1;
+    }
+}
